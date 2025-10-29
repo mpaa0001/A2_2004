a44f428a74ff9351ba7e31dcc3a8a1c2aac392ed
Wegdan <melanie.paaris03@gmail.com>
2025-10-29 12:44:11 +1100

    dijkstras algorithm

--- FIT2004.S2.py
+++ FIT2004.S2.py
@@ -8,11 +8,6 @@ def assign(L, roads, students, buses, D, T):
         adjacent_list[v].append((u, w))
 
 
-    adjacent_list = [[] for _ in range(L)] #adjaceny list built
-    for u, v, w in roads:
-        adjacent_list[u].append((v, w))
-        adjacent_list[v].append((u, w))
-
     B = len(buses)  #numnber of buses
     S = len(students) #number of students
     infinity = float('inf') #infinity value for Dijkstra's algorithm
@@ -25,7 +20,7 @@ def assign(L, roads, students, buses, D, T):
         distances[pickup_point] = 0
         priority_queue = [(0, pickup_point)]
 
-
+#dijkstra's alogrithm, 
         while priority_queue:
             current_distance, current_node = heapq.heappop(priority_queue)
             if current_distance != distances[current_node]:
@@ -36,4 +31,25 @@ def assign(L, roads, students, buses, D, T):
                     distances[neighbor] = new_distance
                     heapq.heappush(priority_queue, (new_distance, neighbor))
 
-                    
+        for student_index, student_location in enumerate(students):
+            if distances[student_location] <= D:
+                reachable_students[bus_index].append(student_index)
+
+
+    total_min = 0
+    total_max = 0 
+    for bus in buses:
+        total_min += bus[1]
+        total_max += bus[2]
+
+    if not (total_min <= T <= total_max):
+        return None 
+
+    
+
+
+
+
+
+
+
