7286c9a652e7109e1856492f400d9ec2b019e3da
Wegdan <melanie.paaris03@gmail.com>
2025-10-29 14:36:06 +1100

    testing and debugging

--- FIT2004.S2.py
+++ FIT2004.S2.py
@@ -34,7 +34,9 @@ def assign(L, roads, students, buses, D, T):
         for student_index, student_location in enumerate(students):
             if distances[student_location] <= D:
                 reachable_students[bus_index].append(student_index)
-
+        
+        for bus_index, reach in enumerate(reachable_students):
+            print(f"Bus {bus_index} can reach students: {reach}")
 
     total_min = 0
     total_max = 0 
@@ -43,6 +45,7 @@ def assign(L, roads, students, buses, D, T):
         total_max += bus[2]
 
     if not (total_min <= T <= total_max):
+        print("Impossible: total_min= ", total_min, "total_max= ", total_max)
         return None #imposttibel total students
     
 
@@ -84,3 +87,33 @@ def assign(L, roads, students, buses, D, T):
 
     backtrack(0, 0)
     return allocation if solution_found[0] else None
+
+
+
+
+# TEST 1:
+
+L = 16
+roads = [
+    (0,1,3), (0,2,5), (0,3,10), (1,4,1), (2,5,2), (5,6,3),
+    (2,7,4), (0,8,1), (0,9,1), (0,10,1), (0,11,1), (6,12,2),
+    (6,13,4), (6,14,3), (7,15,1)
+]
+
+students = [
+    4, 10, 8, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13,
+    5, 7, 7, 7, 7, 7, 15, 15, 7, 4, 8, 9
+]
+
+buses = [
+    (0, 3, 5),   # Bus 0
+    (6, 5, 10),  # Bus 1
+    (15, 5, 10), # Bus 2
+    (6, 5, 10)   # Bus 3
+]
+
+D = 5
+T = 22
+
+result1 = assign(L, roads, students, buses, D, T)
+print("Test 1 Result:", result1)
