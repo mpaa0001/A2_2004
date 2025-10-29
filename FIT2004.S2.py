ae5acc1f9a4b6468ebe690b93441572470b81a25
Wegdan <melanie.paaris03@gmail.com>
2025-10-29 22:16:15 +1100

    max/ min capacticty and totals

--- FIT2004.S2.py
+++ FIT2004.S2.py
@@ -23,6 +23,24 @@ def assign(L, roads, students, buses, D, T):
             buses_at_pickup_point.append([bus_id])
         else:
             buses_at_pickup_point[j].append(bus_id)
+
+
+
+    min_caps = [0] * B
+    max_caps = [0] * B
+    total_min = 0
+    total_max = 0
+    for bus_index in range(B):
+        _, min_cap_bus_index, max_cap_bus_index = buses[bus_index]
+        min_caps[bus_index] = min_cap_bus_index
+        max_caps[bus_index] = max_cap_bus_index
+        total_min += min_cap_bus_index
+        total_max += max_cap_bus_index
+
+    if T < total_min or T > total_max:
+        return None
+    
+    infinity = max(total_max + 1, D _ 1)
        
 
 
