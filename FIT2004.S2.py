89a6b0aa26b1b8d583e501b404ee6a0fd61aeef8
Wegdan <melanie.paaris03@gmail.com>
2025-10-29 20:03:07 +1100

    trying max/min method

--- FIT2004.S2.py
+++ FIT2004.S2.py
@@ -16,7 +16,86 @@ def assign(L, roads, students, buses, D, T):
     infinity = float('inf') #infinity value for Dijkstra's algorithm
 
 
-    reachable_students = [[] for _ in range(B)] #list of reachable students for each bus
+    reachable_by_bus= [[] for _ in range(B)] #list of reachable students for each bus
+    for bus_id in range(B):
+        pickup, _, _ = buses[bus_id]
+        distances = [infinity] * L
+        finalised = [False] * L
+        distances[pickup] = 0
+        priority_queue = [(0, pickup)]
+
+        while priority_queue:
+            current_distance, u = heapq.heappop(priority_queue)
+            if finalised[u]:
+                continue
+            if current_distance > D:
+                break
+            finalised[u] = True
+
+            for v, w in adjacent_list[u]:
+                new_distance = current_distance + w
+                if new_distance <= D and new_distance < distances[v]:
+                    distances[v] = new_distance
+                    heapq.heappush(priority_queue, (new_distance, v))
+
+        for student_id, location_id in enumerate(students):
+            if distances[location_id] <= D:
+                reachable_by_bus[bus_id].append(student_id)
+
+    # for each student, checks which buses can reach them
+    buses_by_student = [[] for _ in range(S)] #list of buses that can reach each student
+    for bus_id, student_list in enumerate(reachable_by_bus):
+        for student_id in student_list:
+            buses_by_student[student_id].append(bus_id)  
+
+
+
+    min_required = 0
+    max_allowed = 0
+    for _, min_cap, max_cap in buses:
+        min_required += min_cap
+        max_allowed += max_cap
+    if T < min_required or T > max_allowed:
+        return None #impossible total students
+    
+
+    allocation = [-1] * S
+    curr_no_students_bus = [0] * B
+    assigned_list = [[] for _ in range(B)] 
+
+    def add_to_bus(student_id, bus_id):
+        allocation[student_id] = bus_id
+        curr_no_students_bus[bus_id] += 1
+        assigned_list[bus_id].append(student_id)
+
+    def remove_from_bus(student_id, bus_id):
+        allocation[student_id] = -1
+        curr_no_students_bus[bus_id] -= 1
+
+    # dfs method
+    def try_assign_student(student_id, visited_buses, visited_students, cap, floor_cap, start_target):
+        for bus_position in range(len(buses_by_student[student_id])):
+            bus_id = buses_by_student[student_id][bus_position]
+            if visited_buses[bus_id]:
+                continue
+            visited_buses[bus_id] = True
+
+            if curr_no_students_bus[bus_id] < cap[bus_id]:
+                add_to_bus(student_id, bus_id)
+                return True
+            
+            if curr_no_students_bus[bus_id] > floor_cap[bus_id] or bus_id == start_target:
+                
+            
+
+
+           
+
+
+
+
+
+
 
 
     for bus_index in range(B):
